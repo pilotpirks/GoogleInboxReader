1204a98b5fe9f864e07ccc54d5dfeb8eb9c45cf3
Your Name <you@example.com>
2022-12-22 14:12:44 -0500

    init

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package gmail_reader
 
 import (
 	"context"
@@ -14,6 +14,10 @@ import (
 	"google.golang.org/api/gmail/v1"
 	"google.golang.org/api/option"
 )
+
+var srv *gmail.Service
+
+const tmplt = "Here is the verification code for your recent login attempt"
 
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
@@ -69,85 +73,102 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func main() {
+func InitReader() error {
 	ctx := context.Background()
 	b, err := os.ReadFile("credentials.json")
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		err = fmt.Errorf("Unable to read client secret file: %v", err)
+		return err
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope /* MailGoogleComScope */)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		err = fmt.Errorf("Unable to parse client secret file to config: %v", err)
+		return err
 	}
 	client := getClient(config)
 
-	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
+	srv, err = gmail.NewService(ctx, option.WithHTTPClient(client))
 	if err != nil {
-		log.Fatalf("Unable to retrieve Gmail client: %v", err)
+		err = fmt.Errorf("Unable to retrieve Gmail client: %v", err)
+		return err
 	}
+
+	return nil
+}
+
+func GetCode() (string, error) {
+	var err error
+	var code string
 
 	msgs, err := srv.Users.Messages.List("me").Do()
 	if err != nil {
-		log.Printf("err: %+s", err)
-		return
+		return code, err
 	}
 
 	for _, v := range msgs.Messages {
 
 		msg, err := srv.Users.Messages.Get("me", v.Id).Format("raw").Do()
 		if err != nil {
-			log.Printf("err: %+s", err)
+			log.Printf("srv.Users.Messages.Get error: %+s", err)
 			continue
 		}
 
-		if !strings.Contains(msg.Snippet, "Here is the verification code for your recent login attempt") {
+		if !strings.Contains(msg.Snippet, tmplt) {
 			continue
 		}
 
 		parts := strings.Split(msg.Snippet, ":")
-		code := strings.Split(parts[1], " ")
-		log.Printf("%v\n", code[1])
+		code = strings.Split(parts[1], " ")[1]
+		// log.Printf("verification code: %s\n", code)
 
-		/* 		msg, err := srv.Users.Messages.Get("me", v.Id).Do()
-		   		if err != nil {
-		   			log.Printf("err: %+s", err)
-		   			continue
-		   		}
-
-		   		for _, h := range msg.Payload.Headers {
-		   			if h.Name != "Subject" {
-		   				continue
-		   			}
-
-		   			if h.Value != "The Appliance Repair Men - Login Verification Code" {
-		   				continue
-		   			}
-
-		   			log.Println(h.Name, h.Value, v.Id, v.Snippet)
-
-		   			decodedData, err := base64.URLEncoding.DecodeString(msg.Payload.Body.Data)
-		   			if err != nil {
-		   				log.Printf("err: %+s", err)
-		   				continue
-		   			}
-
-		   			log.Printf("decodedData: %v\n", decodedData)
-		   			log.Println("---------------------------------------------------------------------------------------------------")
-		   		} */
-
+		break
 	}
 
-	// var msg *gmail.Message
-	// decodedData, _ := base64.URLEncoding.DecodeString(gmailMessageResposne.Raw)
-	// base64.URLEncoding(decodedData, decodedData)
-	// ioutile.WriteFile("message.eml", decodedData, os.ModePerm)
-
-	// Subject: The Appliance Repair Men - Login Verification Code
-	// From: notifications@theappliancerepairmen.com
-
+	return code, err
 }
+
+/* func main() {
+	_ = InitReader()
+	_, _ = GetCode()
+} */
+
+// var msg *gmail.Message
+// decodedData, _ := base64.URLEncoding.DecodeString(gmailMessageResposne.Raw)
+// base64.URLEncoding(decodedData, decodedData)
+// ioutile.WriteFile("message.eml", decodedData, os.ModePerm)
+
+// Subject: The Appliance Repair Men - Login Verification Code
+// From: notifications@theappliancerepairmen.com
+
+/* 		msg, err := srv.Users.Messages.Get("me", v.Id).Do()
+   		if err != nil {
+   			log.Printf("err: %+s", err)
+   			continue
+   		}
+
+   		for _, h := range msg.Payload.Headers {
+   			if h.Name != "Subject" {
+   				continue
+   			}
+
+   			if h.Value != "The Appliance Repair Men - Login Verification Code" {
+   				continue
+   			}
+
+   			log.Println(h.Name, h.Value, v.Id, v.Snippet)
+
+   			decodedData, err := base64.URLEncoding.DecodeString(msg.Payload.Body.Data)
+   			if err != nil {
+   				log.Printf("err: %+s", err)
+   				continue
+   			}
+
+   			log.Printf("decodedData: %v\n", decodedData)
+   			log.Println("---------------------------------------------------------------------------------------------------")
+   		}
+*/
 
 /* Create a New Project.
 Go to APIs and Services.
